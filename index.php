601ef9e008d23fd09d60258ed8e44668e1fcba32
malitha-tishamal <malithatishamal@gmail.com>
2025-06-06 19:01:30 +0530

    fix bugs

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title>Login - MediQ</title>
+    <title>Login - Edulk</title>
     <meta content="" name="description">
     <meta content="" name="keywords">
 
